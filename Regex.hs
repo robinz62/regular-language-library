ca49743d0c48381caaf46ceb4b90f49745b84644
rogerz343 <rogerz888@gmail.com>
2017-12-03 20:59:27 -0500

    regex basics

--- Regex.hs
+++ Regex.hs
@@ -13,36 +13,64 @@ import qualified Data.Set as Set
 
 import Matcher
 
-data Regex = Char (Set Char)
-           | Alt Regex Regex
-           | Seq Regex Regex
-           | Star Regex
-           | Empty
-           | Void
+data Regex a = Single (Set a)
+             | Alt (Regex a) (Regex a)
+             | Seq (Regex a) (Regex a)
+             | Star (Regex a)
+             | Empty
+             | Void
   deriving (Show, Eq)
 
-char :: Char -> Regex
-char = Char . Set.singleton
+single :: Ord a => a -> Regex a
+single = Single . Set.singleton
 
-chars :: String -> Regex
-chars = Char . Set.fromList
+singles :: Ord a => [a] -> Regex a
+singles = Single . Set.fromList
 
+-- all decompositions of a string into two pieces
+splitPair :: [a] -> [([a], [a])]
+splitPair list = do i <- [0..(length list)]
+                    return (splitAt i list)
 
+-- all decompositions of a string into multi-part (nonempty) pieces
+parts :: [a] -> [[[a]]]
+parts [] = [[]]
+parts (h : t) = do ans <- parts t
+                   case ans of
+                     [] -> [[[h]]]
+                     a : as -> [[h] : ans, ((h : a) : as)]
 
+acceptRegex :: Ord a => Regex a -> [a] -> Bool
+acceptRegex (Single set) s = case s of
+  [c] -> Set.member c set
+  _ -> False
+acceptRegex (Alt r1 r2) s = acceptRegex r1 s || acceptRegex r2 s
+acceptRegex (Seq r1 r2) s = any (\(l, r) -> acceptRegex r1 l && acceptRegex r2 r) (splitPair s)
+acceptRegex (Star r) s = any (\try -> all (\substr -> acceptRegex r substr) try) (parts s)
+acceptRegex Empty s = s == []
+acceptRegex Void _ = False
 
+-- regex with an associated alphabet
+data RegexA a = R (Regex a, Set a) deriving (Eq, Show)
 
-instance Matcher Regex where
-  accept :: Regex -> String -> Maybe Bool
-  accept = undefined
+instance Matcher RegexA where
+  accept :: Ord a => RegexA a -> [a] -> Maybe Bool
+  accept (R (regex, alphabet)) str =
+    if foldr (\x acc -> Set.member x alphabet && acc) True str
+      then Just $ acceptRegex regex str
+      else Nothing
 
-  union :: Regex -> Regex -> Maybe Regex
-  union = undefined
+  union :: Ord a => RegexA a -> RegexA a -> Maybe (RegexA a)
+  union (R (r1, alpha1)) (R (r2, alpha2)) =
+    if alpha1 /= alpha2
+      then Nothing
+      else Just $ R (Alt r1 r2, alpha1)
 
-  intersect :: Regex -> Regex -> Maybe Regex
-  intersect = undefined
+  intersect :: Ord a => RegexA a -> RegexA a -> Maybe (RegexA a)
+  intersect r1 r2 = undefined -- Just $ nfaToRegex (intersect (regexToNFA r1) (regexToNFA r2))
 
-  minus :: Regex -> Regex -> Maybe Regex
+  minus :: Ord a => RegexA a -> RegexA a -> Maybe (RegexA a)
   minus = undefined
 
-  fromString :: String -> Maybe Regex
+  fromString :: String -> Maybe (RegexA Char)
   fromString = undefined
--- RegexTest.hs
+++ RegexTest.hs
@@ -15,3 +15,31 @@ import Matcher
 import Regex
 
 -- basically same tests as dfa and nfa
+
+testSplitPair :: Test
+testSplitPair =
+  TestList [
+    (Set.fromList $ splitPair "") ~?= (Set.fromList [("", "")]),
+    (Set.fromList $ splitPair "a") ~?= (Set.fromList [("", "a"), ("a", "")]),
+    (Set.fromList $ splitPair "ab") ~?=
+      (Set.fromList [("", "ab"), ("a", "b"), ("ab", "")]),
+    (Set.fromList $ splitPair "abc") ~?=
+      (Set.fromList [("","abc"),("a","bc"),("ab","c"),("abc","")]),
+    (Set.fromList $ splitPair "abcd") ~?=
+      (Set.fromList [("", "abcd"), ("a", "bcd"), ("ab", "cd"),
+      ("abc", "d"), ("abcd", "")])
+  ]
+
+testParts :: Test
+testParts =
+  TestList [
+    (Set.fromList $ parts "") ~?= (Set.fromList [[]]),
+    (Set.fromList $ parts "a") ~?= (Set.fromList [["a"]]),
+    (Set.fromList $ parts "ab") ~?= (Set.fromList [["ab"], ["a", "b"]]),
+    (Set.fromList $ parts "abc") ~?=
+      (Set.fromList [["abc"], ["a", "bc"], ["ab", "c"], ["a", "b", "c"]]),
+    (Set.fromList $ parts "abcd") ~?=
+      (Set.fromList [["abcd"], ["a", "bcd"], ["a", "b", "cd"],
+      ["a", "b", "c", "d"], ["ab", "cd"], ["ab", "c", "d"],
+      ["abc", "d"], ["a", "bc", "d"]])
+  ]
